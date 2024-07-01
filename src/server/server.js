9a08600d7859d3b54d1eca7cc91b25ae01105c42
Scronkfink <145482124+Scronkfink@users.noreply.github.com>
2024-07-01 14:16:01 -0400

    one more time!

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const mongoose = require('mongoose');
-const { twilioController } = require('./controllers/twilioController');
+const twilioController = require('./controllers/twilioController');
 const userController = require('./controllers/userController');
 const { upload, transcriptionController } = require('./controllers/transcriptionController');
 
