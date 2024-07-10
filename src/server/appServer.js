fd0090c678dd8f77b0d4c3332ee236a769f536c7
Scronkfinkle <145482124+Scronkfink@users.noreply.github.com>
2024-07-10 18:30:58 -0400

    App stuff yk

--- src/server/appServer.js
+++ src/server/appServer.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 // Import the userController
 const userController = require('./appControllers/userController.js');
+const { upload, transcriptionController } = require('./appControllers/transcriptionController.js');
 
 // Route to handle POST requests to /app/signIn
 router.post('/signIn', userController.signIn);
 
 router.post('/signUp', userController.signUp);
+
+router.post('/transcribe', transcriptionController.test);
 
 module.exports = router;
