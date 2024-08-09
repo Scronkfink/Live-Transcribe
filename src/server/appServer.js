b2ba9dda2c1550bdffdedfcbbe1090f936b59441
Scronkfinkle <145482124+Scronkfink@users.noreply.github.com>
2024-08-09 13:19:52 -0400

    File Upload for APP

--- src/server/appServer.js
+++ src/server/appServer.js
@@ -7,6 +7,17 @@ const emailController = require('./appControllers/emailController.js');
 const twilioController = require('./appControllers/twilioController.js');
 const summarizationController = require('./appControllers/summarizationController.js')
 
+const setAudioPath = (req, res, next) => {
+  if (req.file && req.file.path) {
+    res.locals.audioFilePath = req.file.path;
+    console.log(`File uploaded to: ${res.locals.audioFilePath}`);
+  } else {
+    console.error('No file uploaded.');
+    return res.status(400).send('No file uploaded.');
+  }
+  next();
+};
+
 router.post('/signIn', userController.signIn, twilioController.twoFactor);
 router.post('/authentication', userController.authenticate);
 router.post('/signUp', userController.signUp, twilioController.twoFactor);
@@ -16,5 +27,13 @@ router.post("/faceIDSignIn", userController.faceID);
 router.post('/getTranscriptions', userController.getTranscriptions);
 router.post('/getPDF', userController.getPDF);
 router.post('/getSummary', userController.getSummary);
+
+router.post('/uploadFile', upload.single('file'), setAudioPath, userController.createTranscription, transcriptionController.transcribe, summarizationController.summarize, userController.uploadTranscription, twilioController.transcriptionReady, emailController.sendTranscript, (req, res) => {
+  res.send({ transcription: res.locals.transcription });
+});
+
+
+
+
 
 module.exports = router;
